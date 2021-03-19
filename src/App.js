a133b18b6c9a85e67ecdcd1a15374c245dae19ea
Nirawit Naktham <namejoe1568@gmail.com>
2021-03-20 00:10:06 +0700

    No3 finished

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router,Route } from 'react-router-dom'
 //components
-import Navbar from './components/navBar';
+import Navbar from './components/navbar';
 import Slide from './components/slide';
 import Reccom from './components/recommmend';
 import BestSell from './components/bestSell';
